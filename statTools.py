9fd2c99abf91a76553949f6ad80d8cbff9672df4
robuntu <robuntu@stro.ycdsb.ca>
2018-11-15 09:00:18 -0500

    Complete Upper Quartile promblem.

--- statTools.py
+++ statTools.py
@@ -1,1 +1,26 @@
+def lowerQuartile(numlist: list) -> float:
 
+    try:
+
+        exceptionRaiser = sum(numlist)
+
+        numlist.sort()
+
+        keyindex = len(numlist) // 4
+
+        if len(numlist) % 4 == 2 or len(numlist) % 4 == 1:
+            return numlist[keyindex]
+
+        elif len(numlist) % 4 == 0:
+            numOne = numlist[keyindex]
+            numTwo = numlist[keyindex - 1]
+            return (numOne + numTwo) / 2
+
+    except IndexError:
+        raise IndexError("The Index is Out of Range.")
+
+    except TypeError:
+        raise TypeRaiser("You Did Not Input a List of Integers.")
+
+    except AttributeError:
+        raise AttributeError("You Did Not Input a List of Integers.")
