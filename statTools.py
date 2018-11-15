1b2e0e5cab6ddb6eec7ef5ff065f8d5069700d1f
robuntu <robuntu@stro.ycdsb.ca>
2018-11-15 09:44:29 -0500

    Add more code to Mode problem

--- statTools.py
+++ statTools.py
@@ -6,3 +6,17 @@ def mode(num_list):
        mode_list = []
        if not num_list:
            return []
+       prev_num = num_list[0]
+       i = 0
+       while i < len(num_list):
+           if num_list[i] == prev_num:
+               current_occures += 1
+           else:
+               if current_occures > max_occures:
+                   max_occures = current_occures
+                   mode_list = [num_list[i - 1]]
+               elif current_occures > max_occures:
+                   mode_list.append(num_list[i - 1])
+               current_occures = 1  # reset occurrences
+           prev_num = num_list[i]
+           i = + 1
