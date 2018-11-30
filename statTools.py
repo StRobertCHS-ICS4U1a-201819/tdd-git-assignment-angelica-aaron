f1e4f581cd724f2e310af0a4b5c30a8c5028821a
aaron <aaron.ramoscortes19@ycdsbk12.ca>
2018-11-29 21:37:11 -0500

    Problems complete

--- statTools.py
+++ statTools.py
@@ -24,3 +24,47 @@ def lowerQuartile(numlist: list) -> float:
 
     except AttributeError:
         raise AttributeError("You Did Not Input a List of Integers.")
+
+def upperQuartile(numlist: list) -> float:
+
+   try:
+       exceptionRaiser = sum(numlist)
+
+       numlist.sort()
+
+       keyindex = (len(numlist) - 1) - (len(numlist) // 4)
+
+       if len(numlist) % 4 == 2 or len(numlist) % 4 == 1:
+           return numlist[keyindex]
+
+       elif len(numlist) % 4 == 3:
+           numone = numlist[keyindex]
+           numtwo = numlist[keyindex + 1]
+           return (numone + numtwo) / 2
+
+   except IndexError:
+       raise IndexError("The index is out of range.")
+
+   except TypeError:
+       raise TypeError("You did not input a list of integers.")
+
+   except AttributeError:
+       raise AttributeError("You did not input a list of integers.")
+
+def mean(numlist)
+
+   try:
+       listClone = numlist
+
+       numberSum = 0
+
+       for item in listClone:
+
+           numberSum = numberSum + item
+
+       finalNum = numberSum / len(numlist)
+
+       return finalNum
+
+   except:
+       return "Invalid Input"
